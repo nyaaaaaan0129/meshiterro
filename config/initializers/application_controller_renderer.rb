aa822a91273ae1927c9b1d0c9313d638cb8105e6
村田花穂 <muratahanaho@muratakasuinoMacBook-Air.local>
2020-05-02 22:36:45 +0900

    Finish 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '92670cd81ac5264daede769248fc15a788c7a634c25ab3f2bca4827f18dad8974fd4b91ff6e2d35a202f43e4d33bf9b03ffe148a0ed1447f02d863d78de53ebd'
