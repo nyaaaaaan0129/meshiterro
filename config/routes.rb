d673f9305d50a363191746de283e123d09730f51
村田花穂 <muratahanaho@muratakasuinoMacBook-Air.local>
2020-05-03 19:04:04 +0900

    Start 5-4

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root 'post_images#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :post_images, only: [:new, :create, :index, :show]
+      resource :post_comments, only: [:create]
 
 end
